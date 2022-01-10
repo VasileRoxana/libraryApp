01e8f62970a609e1b9b0671e0d76f31d5d719b4c
Andreea Debu <andreea_debu@yahoo.com>
2022-01-11 00:24:52 +0200

    Add link to home page Book and add auth microservice to Eureka Server

--- registration-login-spring-boot-security-thymeleaf-hibernate-master/src/main/java/net/javaguides/springboot/web/MainController.java
+++ registration-login-spring-boot-security-thymeleaf-hibernate-master/src/main/java/net/javaguides/springboot/web/MainController.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.web;
 
+import net.javaguides.springboot.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -24,4 +25,5 @@ public class MainController {
 		//attributes.addAttribute("attribute", "redirectWithRedirectView");
 		return new RedirectView("/books/all");
 	}
+
 }
